040be28577afab365ab9ab1bbb6f1ade6f8c6937
GitHub Action <action@github.com>
2025-04-17 08:48:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/blirhye76/i18next/MyBundle.kt
+++ src/main/kotlin/com/github/blirhye76/i18next/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.blirhye76.i18next
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/blirhye76/i18next/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/blirhye76/i18next/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.blirhye76.i18next.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
